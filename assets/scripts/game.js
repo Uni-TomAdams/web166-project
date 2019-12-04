58e272542c19a44afb3c2522253dc8929b1423d2
PoloSwift <poloswift1337@gmail.com>
2019-12-04 00:50:06 +0000

    Completed game status functionality

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -8,6 +8,24 @@ const gameData = {
 };
 
 
+/*                                                     Helper Functions */
+/*----------------------------------------------------------------------*/
+
+
+function getRandomInteger() {
+    const random = Math.trunc(Math.random() * 10);
+
+    if(random > 6) {
+        return 6;
+    }
+    else if(random < 1) {
+        return 1;
+    }
+    else {
+        return random;
+    }
+}
+
 /*                                                      Light Functions */
 /*----------------------------------------------------------------------*/
 
@@ -69,7 +87,7 @@ function turnLightOff(lightID) {
 *   @Description: Fetches the light state and checks to see if its currently active,
 *   and turns it on when active.
 * */
-function turnLightOn(lightID) {
+function turnLightOn(lightID, colour) {
 
     // Get light URI
     var lightURI = getLightURI(lightID);
@@ -79,6 +97,8 @@ function turnLightOn(lightID) {
         $.ajax({
             url: lightURI,
             type: "PUT",
+            hue: colour,
+            bri: 254,
             data: JSON.stringify({"on": true})
         })
     }
@@ -114,54 +134,105 @@ function applyLightColoursToLightButtons() {
     }
 }
 
+/*
+*   @Params: none;
+*   @Description: An async function used to update the game status board, that informs
+*   the user of the current state of their game level. Two different modes defined.
+* */
 function updateGameStatusBoard(statusType) {
 
     var counter = 0;
 
-    if(statusType == "startLightsCounter") {
+    // A count-down clock from 3 to 0, being the time given to complete the sequence
+    if(statusType == "startLightsCountDown") {
 
         $("#statusTitle").text("Watch the lights in");
         $("#statusCounter").text("3");
 
-        var x = setInterval(function() {
-            $("#statusCounter").text(`${3 - counter}`);
-            counter++;
+        // Return a promise that resolves after the counter has finished
+        return new Promise(resolve => {
+            const x = setInterval(function() {
+                $("#statusCounter").text(`${3 - counter}`);
+                counter++;
 
-            if(counter == 4)  {
-                clearInterval(x);
-                counter = 0;
-            }
-        }, 1000)
+                // End interval clock and resolve
+                if(counter == 4)  {
+                    clearInterval(x);
+                    resolve(true);
+                }
+            }, 1000);
+        });
     }
-    else if(statusType == "startGuessSequenceCountDown") {
+
+    // A count-down clock from 30 to 0, being the time given to complete the sequence
+    if(statusType == "startGuessSequenceCountDown") {
 
         $("#statusTitle").text("What is the sequence?");
         $("#statusCounter").text("30");
 
-        var x = setInterval(function() {
-            $("#statusCounter").text(`${30 - counter}`);
-            counter++
+        // Return a promise that resolves after the counter has finished
+        return new Promise(resolve => {
+            const x = setInterval(function() {
+                $("#statusCounter").text(`${30 - counter}`);
+                counter++;
 
-            if(counter == 31) {
-                clearInterval(x);
-                counter = 0;
-            }
-        }, 1000)
-    }
-    else {
-        console.log("[ERROR] - Invalid statusType in status board");
+                // End interval clock and resolve
+                if(counter == 31) {
+                    clearInterval(x);
+                    resolve(true);
+                }
+            }, 1000)
+        });
     }
 }
 
-function startLevel(level) {
+/*
+*   @Params: level - the current level of the users game session;
+*   @Description: Generates the current levels light sequence which is randomly
+*   generated and stored into the currentLevelLightSequence.
+* */
+function generateRandomLightSequenceByLevel(level) {
 
-    // Start status board
-    updateGameStatusBoard("startLightsCounter");
+    let counter = 0;
 
+    return new Promise(resolve => {
+        const intervalClock = setInterval(function() {
+            let randomNumber = getRandomInteger();
+
+            // Turn random light on
+            //turnLightOn(randomNumber, gameData.lightColours[randomNumber]);
+
+            // Push light ID into the light sequence array
+            gameData.currentLevelLightSequence.push(randomNumber);
+            counter++;
+
+            console.log(randomNumber)
+            // Break-point
+            if(counter == (level + 1)) {
+                clearInterval(intervalClock);
+                resolve(true);
+            }
+        }, 3000);
+    });
+}
+
+/*
+*   @Params: level - the current level of the users game session;
+*   @Description: The entry-point of the specified level, each level will invoke this
+*   function to get the same functionality per level basis.
+* */
+async function startLevel() {
+    await updateGameStatusBoard("startLightsCountDown");
+    console.log("Game Status done...");
+    await generateRandomLightSequenceByLevel(gameData.level);
+    console.log("Light Sequence done...");
 }
 
 function startGame() {
-    turnAllLightsOff();
+    if (turnAllLightsOff) {
+
+    }
+
 }
 
 
@@ -173,11 +244,10 @@ $(document).ready(function()
 {
     applyLightColoursToLightButtons();
 
-    // Waits for game state button to be triggered
+    // Waits for game start button to be triggered
     $("#startGameBtn").click(function() {
 
-        //startGame();
-        startLevel();
+        startLevel(gameData.level);
     });
 });
 
