fb9611ecbcc7bfd842363f6cfb2f926774a8f077
Uni-TomAdams <thomas.adams-3@students.plymouth.ac.uk>
2019-12-12 16:38:59 +0000

    [Bug Fix] Player can not longer click the light buttons while the game is actively generating a sequence.

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -30,7 +30,6 @@ let gameData = {
 *   @Description: Generates a random integer between 1 and 6.
 * */
 function getRandomInteger() {
-
     // Get random float and then truncate to number
     const random = Math.trunc(Math.random() * 10);
 
@@ -54,7 +53,6 @@ function getRandomInteger() {
 *   @Description: Constructs the URL for the interaction of a specified light by ID
 * */
 function getLightURI(lightID) {
-
     const IP = "http://192.168.0.50/api/";
     const username = "stlaB2I6VZ8O80Qepc-1xfmLrHgyTFvB9IGupaQz";
     const lights = "/lights/";
@@ -69,7 +67,6 @@ function getLightURI(lightID) {
 *   state of the light.
 * */
 function checkLightStatus(lightURI) {
-
     // GET light state
     $.getJSON(lightURI, function(lightData) {
         if(lightData["state"]["on"]) {
@@ -86,7 +83,6 @@ function checkLightStatus(lightURI) {
 *   and turns it off when active.
 * */
 function turnLightOff(lightID) {
-
     // Get light URI
     const lightURI = getLightURI(lightID);
 
@@ -107,13 +103,11 @@ function turnLightOff(lightID) {
 *   and turns it on when active.
 * */
 function turnLightOn(lightID, colour) {
-
     // Get light URI
     const lightURI = getLightURI(lightID);
 
     // Check to see if specified light is currently active and toggle
     if(!checkLightStatus(lightURI)) {
-
         $.ajax({
             url: lightURI + "/state/",
             type: "PUT",
@@ -133,8 +127,12 @@ function turnAllLightsOff() {
     }
 }
 
+/*
+*   @Params: none.
+*   @Description: Turns all lights on flashing a red colour for 1s and
+*   then switches itself off.
+* */
 function turnGameOverLightsOn() {
-
     // Turn lights on
     for(var i = 0; i < 6; i++) {
         turnLightOn(i + 1, gameData.gameOverLightColour);
@@ -146,8 +144,12 @@ function turnGameOverLightsOn() {
     }, 1000);
 }
 
+/*
+*   @Params: none.
+*   @Description: Turns all lights on flashing a green colour for 1s and
+*   then switches itself off.
+* */
 function turnPassedLevelLightsOn() {
-
     // Turn lights on
     for(var i = 0; i < 6; i++) {
         turnLightOn(i + 1, gameData.levelPassedLightColour);
@@ -165,17 +167,17 @@ function turnPassedLevelLightsOn() {
 *   and then randomly generating a light between 1 and 6, which is then stored into a local array.
 * */
 function generateRandomLightSequenceByLevel(level) {
-
     let counter = 0;
 
     return new Promise(resolve => {
         const intervalClock = setInterval(function() {
+            // Generate a random number
             let randomNumber = getRandomInteger();
 
             // Turn random light on
             turnLightOn(randomNumber, gameData.hueColours[randomNumber - 1]);
 
-            // Turn random light off
+            // Turn random light off after 1s
             setTimeout(() => {
                 turnLightOff(randomNumber);
             }, 1000)
@@ -183,6 +185,8 @@ function generateRandomLightSequenceByLevel(level) {
             // Push light ID into the light sequence array
             gameData.currentLevelLightSequence.push(randomNumber);
             counter++;
+
+            console.log(randomNumber);
 
             // Break-point
             if(counter == (level + 1)) {
@@ -204,10 +208,14 @@ function generateRandomLightSequenceByLevel(level) {
 *   if the lightsColours object is mutated. This does not update the actual hue lights via HTTP request.
 * */
 function applyLightColoursToLightButtons() {
-
     for(var i = 0; i < 6; i++) {
-
         $(`#${i + 1}`).css("background-color", `${gameData.lightColours[i]}`);
+    }
+}
+
+function disableLightButtons(doDisable) {
+    for(var i = 0; i < 6; i++) {
+        $(`#${i + 1}`).prop('disabled', doDisable);
     }
 }
 
@@ -218,23 +226,20 @@ function applyLightColoursToLightButtons() {
 *
 *   There are two different status types:
 *   1) startLightsCountDown - A 3 second countdown clock informing the player to watch the lights.
-*   2) startGuessSequenceCountDown - A 30 second countdown clock informing the player of how long is
-*      is left to answer the current levels sequence correctly, else the game will end.
+*   2) startGuessSequenceCountDown - A 30 second countdown clock informing the player how long they
+*      have left to answer the current levels sequence correctly, else the game will end.
 * */
 function updateGameStatusBoard(statusType) {
-
-    var counter = 0;
+    let counter = 0;
 
     // A count-down clock from 3 to 0, being the time given to complete the sequence
     if(statusType == "startLightsCountDown") {
-
         // Inform player of countdown timer
         $("#statusTitle").text("Watch the lights in");
         $("#statusCounter").text("3");
 
         // Returns a promise that resolves after the counter has finished
         return new Promise(resolve => {
-
             // A clock decrementing the countdown p/s
             const x = setInterval(function() {
                 $("#statusCounter").text(`${3 - counter}`);
@@ -251,7 +256,6 @@ function updateGameStatusBoard(statusType) {
 
     // A count-down clock from 30 to 0, being the time given to complete the sequence
     if(statusType == "startGuessSequenceCountDown") {
-
         // Inform player of countdown timer
         $("#statusTitle").text("What is the sequence?");
         $("#statusCounter").text("30");
@@ -282,9 +286,7 @@ function updateGameStatusBoard(statusType) {
 *   against the generated sequence.
 * */
 function lightButtonEventHandler(id) {
-
     $(`#${id}`).click(() => {
-
         // Check to see if the count down clock is active
         if(!gameData.isCountDownStillActive) return endGame();
 
@@ -315,13 +317,14 @@ function lightButtonEventHandler(id) {
 * */
 function levelPassed() {
     setTimeout(() => {
+        // Inform player of level passed
         $("#statusTitle").text("Correct!");
         $("#statusCounter").text("Next Level...");
         $("#currentLevel").text(gameData.level + 1);
 
         turnPassedLevelLightsOn();
 
-        // Reset needed settings
+        // Reset data
         gameData = {
             ...gameData,
             level: gameData.level + 1,
@@ -344,8 +347,10 @@ function levelPassed() {
 * */
 function endGame() {
     setTimeout(() => {
+        // Inform player of game end
         $("#statusTitle").text("Incorrect!");
         $("#statusCounter").text("GAME OVER");
+        turnGameOverLightsOn();
 
         // Update highest level and display
         if(gameData.level > gameData.highestScore) {
@@ -354,8 +359,7 @@ function endGame() {
             $("#currentHighestScore").text(`${gameData.highestScore}`);
         }
 
-        turnGameOverLightsOn();
-
+        // Reset data
         gameData = {
             ...gameData,
             level: 1,
@@ -375,10 +379,14 @@ function endGame() {
 *   function to get the same functionality per level.
 * */
 async function startLevel(level) {
+    disableLightButtons(true);
+    console.log("Disabling light buttons...");
     await updateGameStatusBoard("startLightsCountDown");
     console.log("Start light countdown done...");
     await generateRandomLightSequenceByLevel(level);
     console.log("Light sequence done...");
+    disableLightButtons(false);
+    console.log("Enabling light buttons...");
     await updateGameStatusBoard("startGuessSequenceCountDown");
     console.log("Start guess sequence countdown done...");
 }
